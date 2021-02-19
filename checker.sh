984332eecf6e31144d15bb9f90463addec6cd309
Amey Vikram Singh <41503174+amey4753@users.noreply.github.com>
2021-02-19 13:36:55 +0530

    Update checker.sh

--- checker.sh
+++ checker.sh
@@ -2,7 +2,7 @@
 
 URL="http://ec2-35-154-44-39.ap-south-1.compute.amazonaws.com/"
 
-response=$(curl -s -w "%{http_code}" $URL)
+response=$(curl -s -w "%{http_code}" $URL) # getting http code from url 
 
 http_code=$(tail -n1 <<< "$response")  # get the last line
 content=$(sed '$ d' <<< "$response")   # get all but the last line which contains the status code
