721c71f8cb07084c304b5c48bd8ffb8fbec1d996
RKS <rahulkrs2100@gmail.com>
2021-02-19 13:26:01 +0530

    adding checker script

---
+++ checker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+URL="http://ec2-35-154-44-39.ap-south-1.compute.amazonaws.com/"
+
+response=$(curl -s -w "%{http_code}" $URL)
+
+http_code=$(tail -n1 <<< "$response")  # get the last line
+content=$(sed '$ d' <<< "$response")   # get all but the last line which contains the status code
+
+echo "$http_code"
+echo "$content"
